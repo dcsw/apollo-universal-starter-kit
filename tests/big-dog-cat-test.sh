7dcd0aaf4cca205abe441035a4e2188ff4d0f359
DeanC <>
2018-03-17 14:18:04 -0400

    Add (back) big-dog-cat-test.

---
+++ tests/big-dog-cat-test.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+yarn cli deletemodule dog
+yarn cli deletemodule cat
+yarn cli -- addmodule dog
+yarn cli -- addmodule cat
+yarn cli -- link-modules dog cat
+rm dev-db.sqlite3
+yarn seed
+yarn watch
